6bfc51f1c6529bafa2d699bfa2b57620f1f3c5fb
neyazahmad786 <neyazahmad001@gmail.com>
2018-12-31 13:30:10 +0530

    User Login Page, Dashboard Page Add

---
+++ UptronWeb/Controllers/UserLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UptronWeb.Controllers
+{
+    public class UserLoginController : CommonController
+    {
+        // GET: UserLogin
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
