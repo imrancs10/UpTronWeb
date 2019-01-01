7f809f73d3f71dd67792d03b4947beddb30d8d80
neyazahmad786 <neyazahmad001@gmail.com>
2019-01-01 13:20:19 +0530

    Employee Profile page

---
+++ UptronWeb/Controllers/EmployeeProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UptronWeb.Controllers
+{
+    public class EmployeeProfileController : CommonController
+    {
+        // GET: EmployeeProfile
+        public ActionResult EmployeeProfile()
+        {
+            return View();
+        }
+    }
+}
